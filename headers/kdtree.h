675fcaa9fce126aa8a3eb0fce9a0c714a1a381d7
Gabriel L. P. Abreu <gabriel.longo.longo@gmail.com>
2017-05-22 07:31:53 -0300

    Initial Commit

---
+++ headers/kdtree.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   kdtree.h
+ * Author: titan
+ *
+ * Created on 22 de Maio de 2017, 06:29
+ */
+
+#ifndef KDTREE_H
+#define KDTREE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* KDTREE_H */
+
