430c104b9f125e358eb5aebf40b8b7e6aad0b9db
Gabriel L. P. Abreu <gabriel.longo.longo@gmail.com>
2017-05-23 14:59:05 -0300

    Changing 'author' fields

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *
  */
 
 /* 
  * File:   main.c
- * Author: titan
+ * Author: Gabriel L. P. Abreu
  *
  * Created on 21 de Maio de 2017, 20:14
  */
