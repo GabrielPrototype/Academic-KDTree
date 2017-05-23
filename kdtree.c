fe99703c6a8ba427b6fac5c7298cea4902fc3aae
Gabriel L. P. Abreu <gabriel.longo.longo@gmail.com>
2017-05-23 17:55:33 -0300

    Add dotInit() function

--- kdtree.c
+++ kdtree.c
@@ -2,3 +2,15 @@
  *
  */
 
+#include <stdlib.h>
+
+#include "headers/kdtree.h"
+
+
+Dot *dotInit(int x, int y, char *info) {
+    Dot *newdot = malloc(sizeof(Dot));
+    newdot->xcord = x;
+    newdot->ycord = y;
+    strncpy(newdot->tag, info, TAGLEN);
+    return newdot;
+}
--- main.c
+++ main.c
@@ -12,11 +12,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "headers/kdtree.h"
+
 /*
  * 
  */
 int main(int argc, char** argv) {
-
+    
+    
+    Dot *test = dotInit(2,4,"TESTE");
+    
+    printf("%d %d %s", test->xcord, test->ycord, test->tag);
     return (EXIT_SUCCESS);
 }
 
