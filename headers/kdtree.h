10de1e73138bcaf96bcf0628cda226c6c6421e86
Gabriel L. P. Abreu <gabriel.longo.longo@gmail.com>
2017-05-23 17:30:54 -0300

    Add Bi-dimensional KDTree Structs

--- headers/kdtree.h
+++ headers/kdtree.h
@@ -12,7 +12,27 @@
 #ifndef KDTREE_H
 #define KDTREE_H
 
+#include <string.h>
+#include <math.h>
+
+#define TAGLEN 35
+
+typedef struct {
+    int xcord, ycord;
+    char tag[TAGLEN];
+} Dot;
+
+typedef struct {
+    Dot dotinfo;
+    char dif;
+    KDTreeNode *left;
+    KDTreeNode *right;
+} KDTreeNode;
+
+typedef struct {
+    KDTreeNode *info;
+    KDStack *next;
+} KDStack;
 
 
 #endif /* KDTREE_H */
-
