36d49512358baf3bebb4ecbf43ce4187ed09ba15
Gabriel L. P. Abreu <gabriel.longo.longo@gmail.com>
2017-05-31 17:13:40 -0300

    Add util_functions.c function headers

---
+++ headers/util_functions.h
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   util_functions.h
+ * Author: hflab
+ *
+ * Created on 31 de Maio de 2017, 16:58
+ */
+
+#ifndef UTIL_FUNCTIONS_H
+#define UTIL_FUNCTIONS_H
+
+char* fgetsplus(char *restrict str, int count, FILE *restrict stream);
+
+double randFrom(double min, double max);
+#endif /* UTIL_FUNCTIONS_H */
+
