255a0e2623741cadc9cf5cce41efc28ae1486181
Gabriel L. P. Abreu <gabriel.longo.longo@gmail.com>
2017-06-01 01:07:41 -0300

    Add header to strToUpper()

--- headers/util_functions.h
+++ headers/util_functions.h
@@ -1,21 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 
 /* 
  * File:   util_functions.h
- * Author: hflab
+ * Author: Gabriel L. P. Abreu
  *
- * Created on 31 de Maio de 2017, 16:58
  */
 
 #ifndef UTIL_FUNCTIONS_H
 #define UTIL_FUNCTIONS_H
 
+
+
 char* fgetsplus(char *restrict str, int count, FILE *restrict stream);
 
 double randFrom(double min, double max);
+
+void strToUpper(char s[], size_t maxsize);
 #endif /* UTIL_FUNCTIONS_H */
 
