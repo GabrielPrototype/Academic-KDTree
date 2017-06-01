f4f1b0635daa0fd22a107ce149c4d9eba9ed7788
Gabriel L. P. Abreu <gabriel.longo.longo@gmail.com>
2017-06-01 01:10:09 -0300

    other tests moved to test.c

--- tests.c
+++ tests.c
@@ -6,34 +6,23 @@
 #include <stdio.h>
 
 #include "headers/kdtree.h"
+#include "headers/util_functions.h"
 
+void tests();
+void autoInsertTest();
+void insertTest();
 
-//void graphvizTest() {
-//
-//    Agraph_t* G;
-//    GVC_t* gvc;
-//    gvc = gvContext();
-//    G = createGraph();
-//    gvLayout(gvc, G, "dot");
-//    drawGraph(G);
-//    gvFreeLayout(gvc, g);
-//    agclose(G);
-//    gvFreeContext(gvc);
-//
-//}
-
-int _print_t(KDTreeNode *subroot, int is_left, int offset, int depth, char s[30][255])
-{
+int _print_t(KDTreeNode *subroot, int is_left, int offset, int depth, char s[30][255]) {
     char b[40];
     int width = 16;
 
     if (!subroot) return 0;
 
-    sprintf(b,"(%03.2f;%03.2f)",subroot->dotinfo.xcord,subroot->dotinfo.ycord);
+    sprintf(b, "(%03.2f;%03.2f)", subroot->dotinfo.xcord, subroot->dotinfo.ycord);
 
-    int left  = _print_t(subroot->left,  1, offset,                depth + 1, s);
+    int left = _print_t(subroot->left, 1, offset, depth + 1, s);
     int right = _print_t(subroot->right, 0, offset + left + width, depth + 1, s);
-    
+
 
     for (int i = 0; i < width; i++)
         s[2 * depth][offset + left + i] = b[i];
@@ -41,25 +30,24 @@ int _print_t(KDTreeNode *subroot, int is_left, int offset, int depth, char s[30]
     if (depth && is_left) {
 
         for (int i = 0; i < width + right; i++)
-            s[2 * depth - 1][offset + left + width/2 + i] = '-';
+            s[2 * depth - 1][offset + left + width / 2 + i] = '-';
 
-        s[2 * depth - 1][offset + left + width/2] = '+';
-        s[2 * depth - 1][offset + left + width + right + width/2] = '+';
+        s[2 * depth - 1][offset + left + width / 2] = '+';
+        s[2 * depth - 1][offset + left + width + right + width / 2] = '+';
 
     } else if (depth && !is_left) {
-        
-        for (int i = 0; i < left + width; i++)
-            s[2 * depth - 1][offset - width/2 + i] = '-';
 
-        s[2 * depth - 1][offset + left + width/2] = '+';
-        s[2 * depth - 1][offset - width/2 - 1] = '+';
+        for (int i = 0; i < left + width; i++)
+            s[2 * depth - 1][offset - width / 2 + i] = '-';
+
+        s[2 * depth - 1][offset + left + width / 2] = '+';
+        s[2 * depth - 1][offset - width / 2 - 1] = '+';
     }
 
     return left + width + right;
 }
 
-void print_t(KDTreeNode *root)
-{
+void print_t(KDTreeNode *root) {
     int lines = 30;
     char s[lines][255];
     for (int i = 0; i < lines; i++)
@@ -69,4 +57,57 @@ void print_t(KDTreeNode *root)
 
     for (int i = 0; i < lines; i++)
         printf("%s\n", s[i]);
+}
+
+void autoInsertTest() {
+
+    KDTreeNode *root = NULL;
+    Dot testdot;
+    int i;
+    for (i = 0; i < 30; i++) {
+        testdot.xcord = randFrom(-99.0, 99.0);
+        testdot.ycord = randFrom(-99.0, 99.0);
+        strncpy(testdot.tag, " ", TAGLEN);
+        kdtreeInsert(&root, testdot);
+    }
+    //preOrden(root);
+    printf("\n\n");
+    treePrinter(root);
+}
+
+void insertTest() {
+
+    double xcord, ycord;
+    char str[TAGLEN];
+
+    KDTreeNode *root = NULL;
+    Dot testdot;
+
+    printf("Insira a descrição:");
+    fgetsplus(str, TAGLEN, stdin);
+
+    while (strcmp(str, "exit")) {
+        printf("insira os pontos: ");
+        scanf(" %lf %lf", &xcord, &ycord);
+        testdot.xcord = xcord;
+        testdot.ycord = ycord;
+        strncpy(testdot.tag, str, TAGLEN);
+        kdtreeInsert(&root, testdot);
+        printf("Insira a descrição:");
+        fgetsplus(str, TAGLEN, stdin);
+    }
+
+    preOrden(root);
+    printf("\n");
+    treePrinter(root);
+
+    getchar();
+};
+
+void tests() {
+
+    Dot *test = dotInit(2, 4, "TESTE");
+    printf("%f %f %s\n", test->xcord, test->ycord, test->tag);
+    KDTreeNode *nodetest = kdnodeInit(test, 0);
+    getchar();
 }
